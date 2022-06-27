371a3cfa601be04f2c01c348ec0f92daa5ee2417
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-06-27 18:55:49 +0300

    API update

--- experiment/ctx.py
+++ experiment/ctx.py
@@ -1,7 +1,7 @@
 from arekit.common.entities.str_fmt import StringEntitiesFormatter
 from arekit.contrib.experiment_rusentrel.connotations.provider import RuSentiFramesConnotationProvider
 from arekit.contrib.networks.core.input.ctx_serialization import NetworkSerializationContext
-from arekit.contrib.networks.embeddings.base import Embedding
+from arekit.contrib.networks.embedding import Embedding
 from arekit.contrib.source.rusentiframes.collection import RuSentiFramesCollection
 from arekit.processing.pos.base import POSTagger
 
