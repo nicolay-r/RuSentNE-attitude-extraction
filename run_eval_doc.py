3a925a21a07ecb4d924bcedcffc191b55fe43bee
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-07-05 11:16:47 +0300

    Updated: completed implementation

--- run_eval_doc.py
+++ run_eval_doc.py
@@ -82,7 +82,7 @@ def __extract_text_opinions_from_test(test_view, label_scaler, default_label):
     return text_opinion_by_id, text_opinion_by_row_id
 
 
-def __gather_opinion_and_group_ids_from_etalon(etalon_view, label_scaler, default_label):
+def __gather_opinion_and_group_ids_from_view(view, label_scaler, default_label):
     """ Из связок берем только первое отношение и считаем его Opinion.
         А также группируем TextOpinons относительно первого id.
     """
@@ -90,8 +90,8 @@ def __gather_opinion_and_group_ids_from_etalon(etalon_view, label_scaler, defaul
 
     opinion_by_row_id = OrderedDict()
     text_opinion_ids_by_row_id = OrderedDict()
-    etalon_opinions_by_doc_id = OrderedDict()
-    for linkage in tqdm(etalon_view.iter_rows_linked_by_text_opinions()):
+    opinions_by_doc_id = OrderedDict()
+    for linkage in tqdm(view.iter_rows_linked_by_text_opinions()):
 
         first_row = linkage[0]
         first_row_id = first_row["id"]
@@ -103,55 +103,89 @@ def __gather_opinion_and_group_ids_from_etalon(etalon_view, label_scaler, defaul
             __row_to_text_opinion(row, label_scaler, default_label).TextOpinionID for row in linkage
         ]
 
-        if doc_id not in etalon_opinions_by_doc_id:
-            etalon_opinions_by_doc_id[doc_id] = []
-        etalon_opinions_by_doc_id[doc_id].append(opinion)
+        if doc_id not in opinions_by_doc_id:
+            opinions_by_doc_id[doc_id] = []
+        opinions_by_doc_id[doc_id].append(opinion)
 
-    return opinion_by_row_id, text_opinion_ids_by_row_id, etalon_opinions_by_doc_id
+    return opinion_by_row_id, text_opinion_ids_by_row_id, opinions_by_doc_id
 
 
-def __compose_test_opinions_by_doc_id(etalon_opinions_by_row_id, etalon_text_opinion_ids_by_row_id,
-                                      test_opinions_by_id, label_scaler, filter_by_label_func):
-    assert(callable(filter_by_label_func))
+def __vote_label_func(labels):
+    """ Используем метод голосования.
+    """
+    assert(isinstance(labels, list))
 
-    used = set()
+    vote_label = sum(labels)
+    if vote_label < 0:
+        vote_label = -1
+    if vote_label > 0:
+        vote_label = 1
+
+    return vote_label
+
+
+def __compose_test_opinions_by_doc_id(etalon_opinions_by_row_id,
+                                      etalon_text_opinion_ids_by_row_id,
+                                      test_opinions_by_row_id,
+                                      test_text_opinion_ids_by_row_id,
+                                      test_text_opinions_by_id,
+                                      label_scaler,
+                                      filter_opinion_func,
+                                      labels_to_label_func):
+
+    def __try_register_opinion(source_value, target_value, existed_tids):
+        assert(isinstance(source_value, str))
+        assert(isinstance(target_value, str))
+        assert(isinstance(existed_tids, list))
+
+        # если нет tids, то тогда нет смысла составлять отношение.
+        if len(existed_tids) == 0:
+            return
+
+        # используем метод голосования.
+        labels = [label_scaler.label_to_int(test_text_opinions_by_id[tid].Sentiment) for tid in existed_tids]
+        actual_label = labels_to_label_func(labels)
+
+        # создаем Opinion и фиксируем его.
+        opinion = Opinion(source_value=source_value,
+                          target_value=target_value,
+                          sentiment=label_scaler.int_to_label(actual_label))
+
+        if not filter_opinion_func(opinion):
+            return
+
+        # регистрируем для doc_id.
+        doc_id = test_text_opinions_by_id[existed_tids[0]].DocID
+        if doc_id not in test_opinions_by_doc_id:
+            test_opinions_by_doc_id[doc_id] = []
+        test_opinions_by_doc_id[doc_id].append(opinion)
 
     test_opinions_by_doc_id = {}
+
+    used_tids = set()
     for row_id, etalon_opinion in etalon_opinions_by_row_id.items():
         tid_list = etalon_text_opinion_ids_by_row_id[row_id]
 
-        existed_tids = [tid for tid in tid_list if tid in test_opinions_by_id]
+        existed_tids = [tid for tid in tid_list if tid in test_text_opinions_by_id]
 
-        if len(existed_tids) == 0:
-            continue
+        # отмечаем как просмотренные.
+        for tid in existed_tids:
+            used_tids.add(tid)
 
-        labels = [label_scaler.label_to_int(test_opinions_by_id[tid].Sentiment) for tid in existed_tids]
-
-        # используем метод голосования
-        vote_label = sum(labels)
-        if vote_label < 0:
-            vote_label = -1
-        if vote_label > 0:
-            vote_label = 1
-
-        if not filter_by_label_func(vote_label):
-            continue
-
-        test_opinion = Opinion(source_value=etalon_opinion.SourceValue,
+        __try_register_opinion(source_value=etalon_opinion.SourceValue,
                                target_value=etalon_opinion.TargetValue,
-                               sentiment=label_scaler.int_to_label(vote_label))
+                               existed_tids=existed_tids)
 
-        doc_id = test_opinions_by_id[existed_tids[0]].DocID
+    # учитываем оставшиеся.
+    for row_id, test_opinion in test_opinions_by_row_id.items():
+        tid_list = test_text_opinion_ids_by_row_id[row_id]
 
-        if doc_id not in test_opinions_by_doc_id:
-            test_opinions_by_doc_id[doc_id] = []
-        test_opinions_by_doc_id[doc_id].append(test_opinion)
+        # выбираем только те, которые не были использованы ранее.
+        existed_tids = list(filter(lambda tid: tid not in used_tids, tid_list))
 
-        # отмечаем как used.
-        for tid in tid_list:
-            used.add(test_opinions_by_id[tid])
-
-    # TODO. Осталось учесть тех, что нет в Etalon.
+        __try_register_opinion(source_value=test_opinion.SourceValue,
+                               target_value=test_opinion.TargetValue,
+                               existed_tids=existed_tids)
 
     return test_opinions_by_doc_id
 
@@ -185,9 +219,11 @@ def opinions_per_document_result_evaluation(
         test_view=test_view, label_scaler=label_scaler, default_label=no_label)
 
     etalon_opinions_by_row_id, etalon_text_opinion_ids_by_row_id, etalon_opinions_by_doc_id = \
-        __gather_opinion_and_group_ids_from_etalon(etalon_view=etalon_view,
-                                                   label_scaler=label_scaler,
-                                                   default_label=no_label)
+        __gather_opinion_and_group_ids_from_view(view=etalon_view, label_scaler=label_scaler, default_label=no_label)
+
+    test_opinions_by_row_id, test_text_opinion_ids_by_row_id, _ = \
+        __gather_opinion_and_group_ids_from_view(view=test_view, label_scaler=label_scaler, default_label=no_label)
+
     assign_labels(filename=test_predict_filename,
                   text_opinions=test_text_opinions_by_id.values(),
                   row_id_to_text_opin_id_func=lambda row_id: test_text_opinions_by_row_id[row_id].TextOpinionID,
@@ -196,9 +232,12 @@ def opinions_per_document_result_evaluation(
     test_opinions_by_doc_id = __compose_test_opinions_by_doc_id(
         etalon_opinions_by_row_id=etalon_opinions_by_row_id,
         etalon_text_opinion_ids_by_row_id=etalon_text_opinion_ids_by_row_id,
-        test_opinions_by_id=test_text_opinions_by_id,
+        test_opinions_by_row_id=test_opinions_by_row_id,
+        test_text_opinion_ids_by_row_id=test_text_opinion_ids_by_row_id,
+        test_text_opinions_by_id=test_text_opinions_by_id,
         label_scaler=label_scaler,
-        filter_by_label_func=lambda label: label_scaler.uint_to_label(label) != no_label)     # отбрасываем нейтральные
+        filter_opinion_func=lambda opinion: opinion.Sentiment != no_label,      # не берем те, что c NoLabel
+        labels_to_label_func=__vote_label_func)                                 # создаем на основе метода голосования.
 
     synonyms = StemmerBasedSynonymCollection(iter_group_values_lists=[],
                                              stemmer=MystemWrapper(),
@@ -208,7 +247,8 @@ def opinions_per_document_result_evaluation(
     cmp_pairs_iter = OpinionCollectionsToCompareUtils.iter_comparable_collections(
         doc_ids=test_opinions_by_doc_id.keys(),
         read_etalon_collection_func=lambda doc_id: OpinionCollection(
-            opinions=etalon_opinions_by_doc_id[doc_id],
+            # В некоторых случаях может быть ситуация, что в эталонной разметке для документа отсутствуют данные.
+            opinions=etalon_opinions_by_doc_id[doc_id] if doc_id in etalon_opinions_by_doc_id else [],
             synonyms=synonyms,
             error_on_duplicates=False,
             error_on_synonym_end_missed=True),
