6141c58d87d6e1d9ddb187caa34653b0fce3ae92
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-07-14 13:50:48 +0300

    Fixed API update

--- run_train_nn.py
+++ run_train_nn.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     train_nn(output_dir="_out",
              model_log_dir="_model",
              model_name=ModelNames.AttSelfPZhouBiLSTM,
-             split_source="data/split_fixed.txt")
+             split_filepath="data/split_fixed.txt")
