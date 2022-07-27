20065a5892ef71fcfd3401bb3b32ec217c01e239
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-07-27 11:48:06 +0300

    Removed non used methods

--- experiment/io.py
+++ experiment/io.py
@@ -1,5 +1,3 @@
-import os
-
 from arekit.contrib.utils.model_io.tf_networks import DefaultNetworkIOUtils
 
 
@@ -21,15 +19,8 @@ class CustomExperimentSerializationIO(DefaultNetworkIOUtils):
         super(CustomExperimentSerializationIO, self).__init__(exp_ctx=exp_ctx)
         self.__output_dir = output_dir
 
-    def __create_annot_input_target(self, doc_id, data_type):
-        filename = "annot_input_d{doc_id}_{data_type}.txt".format(doc_id=doc_id, data_type=data_type.name)
-        return os.path.join(self._get_target_dir(), filename)
-
     def _get_experiment_sources_dir(self):
         return self.__output_dir
-
-    def create_opinion_collection_target(self, doc_id, data_type, check_existance=False):
-        return self.__create_annot_input_target(doc_id=doc_id, data_type=data_type)
 
 
 class CustomExperimentTrainIO(DefaultNetworkIOUtils):
