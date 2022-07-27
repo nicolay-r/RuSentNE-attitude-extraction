ba1adc57a9fac4ca0716f52db70a8dbc6ba7f589
Nicolay Rusnachenko <kolyarus@yandex.ru>
2022-07-27 17:20:37 +0300

    Removed

--- run_predict_nn.py
+++
@@ -1,6 +0,0 @@
-from arekit.contrib.networks.enum_name_types import ModelNames
-from models.nn.predict import predict_nn
-
-
-if __name__ == '__main__':
-    predict_nn(extra_name_suffix="nn", model_name=ModelNames.CNN, output_dir="_out")
